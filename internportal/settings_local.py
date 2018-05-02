43b143668a5a797f965db4b1e134ecc0b4899840
Seah Chin Ying <scy.6char@gmail.com>
2018-05-03 01:08:31 +0800

    travis testing

---
+++ internportal/settings_local.py
@@ -0,0 +1,15 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'internportal',
+        'USER': 'internportal',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+
+BASE_URL = 'localhost:8000'
+
+DEBUG = True
