ef41fe391f1fc2923367324c798035dcf21d99f8
Seah Chin Ying <scy.6char@gmail.com>
2018-05-03 23:04:41 +0800

    2 basic models Review and Company added, along with view and form

--- reviewsportal/models.py
+++ reviewsportal/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=200, unique=True, db_index=True)
+    site = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User, blank=True)
+    title = models.CharField(max_length=200, null=True, blank=True)
+    body = models.TextField()
--- reviewsportal/views.py
+++ reviewsportal/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, DetailView, ListView, FormView
 
-# Create your views here.
+from reviewsportal.forms import CompanyCreateForm, ReviewCreateForm
+from reviewsportal.models import Company, Review
+
+
+class CompanyListView(ListView):
+    model = Company
+    template_name = "companies/list.html"
+    context_object_name = "companies"
+
+    def get_queryset(self):
+        qs = Company.objects.all()
+        print(qs)
+        return qs
+
+
+class CompanyDetailView(DetailView):
+    model = Company
+    template_name = "companies/detail.html"
+
+
+class CompanyCreateView(FormView):
+    template_name = "companies/new.html"
+    success_url = reverse_lazy("reviews:company-list")
+
+    def get_form_class(self):
+        return CompanyCreateForm
+
+    def form_valid(self, form):
+        form.save()
+        return super(CompanyCreateView, self).form_valid(form)
+
+
+class ReviewsListView(ListView):
+    pass
+
+
+class ReviewCreateView(FormView):
+    template_name = "reviews/new.html"
+    success_url = reverse_lazy("reviews:company-list")
+
+    def get_form_class(self):
+        return ReviewCreateForm
