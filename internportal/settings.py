36fb1a30879cee400e28b675ac8df0fc05e95368
Seah Chin Ying <scy.6char@gmail.com>
2018-05-03 23:49:12 +0800

    static dir added for staticfiles

--- internportal/settings.py
+++ internportal/settings.py
@@ -131,6 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
