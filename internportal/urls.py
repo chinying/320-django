8c2bff08b4daf6e106236ec47adc1d30328807b7
Seah Chin Ying <scy.6char@gmail.com>
2018-05-03 23:01:38 +0800

    urls added for templates

--- internportal/urls.py
+++ internportal/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r"^$", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    url(r"^about/$", TemplateView.as_view(template_name="pages/about.html"), name="about"),
+    # User management
+
+    # Your stuff: custom urls includes go here
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^app/', include('reviewsportal.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
