b0402678671ace3ba861086517425144c4eeb9dd
Seah Chin Ying <scy.6char@gmail.com>
2018-05-12 16:41:19 +0800

    change add review form rating values to *20 multiplier

--- reviewsportal/forms.py
+++ reviewsportal/forms.py
@@ -22,12 +22,12 @@ class CompanyCreateForm(forms.ModelForm):
 
 class ReviewCreateForm(forms.Form):
     rating_choices = (
-        ("0", "0"),
-        ("20", "1"),
-        ("40", "2"),
-        ("60", "3"),
-        ("80", "4"),
-        ("100", "5"),
+        (0, "0"),
+        (20, "1"),
+        (40, "2"),
+        (60, "3"),
+        (80, "4"),
+        (100, "5"),
     )
 
     company = forms.CharField(required=True, widget=forms.TextInput())
