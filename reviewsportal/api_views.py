1821d7e8ac17e6429009fd667fb28f5cd9b5fb10
Seah Chin Ying <scy.6char@gmail.com>
2018-05-12 18:41:40 +0800

    added API view for sorting reviews

--- reviewsportal/api_views.py
+++ reviewsportal/api_views.py
@@ -1,8 +1,17 @@
+from django.http import Http404
+
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import APIView
 
 from reviewsportal.models import Company
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 30
 
 
 class CompanyListView(APIView):
@@ -14,3 +23,18 @@ class CompanyListView(APIView):
             if name:
                 resp = Company.objects.filter(name__icontains=name).values('id', 'name')
         return Response(resp, status=status.HTTP_200_OK)
+
+
+class GetCompanyReviews(APIView):
+    pagination_class = StandardResultsSetPagination
+
+    def get(self, request, *args, **kwargs):
+        query_params = request.query_params
+        if 'id' in query_params:
+            try:
+                company = Company.objects.get(pk=query_params.get('id'))
+                resp = company.get_reviews().values()
+                return Response(resp, status=status.HTTP_200_OK)
+            except Company.DoesNotExist:
+                raise Http404
+        return Response({'': ''}, status=status.HTTP_400_BAD_REQUEST)
